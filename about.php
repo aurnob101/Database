a0dff8a6904b5433bbd40bf12bc1f45fa464a6d8
Raisa-Nuzhat <106836890+Raisa-Nuzhat@users.noreply.github.com>
2023-06-03 20:20:01 +0600

    Add files via upload
    about.php 
     create_session_query.php 
    index.php 
     login.php 
    logout.php 
    total_percantage.php 
    view_course_query.php

---
+++ about.php
@@ -0,0 +1,246 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>cusas-about</title>
+	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="css/flexslider.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.theme.default.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.rateyo.css"/>
+	<link rel="stylesheet" type="text/css" href="css/jquery.mmenu.all.css" />
+	<link rel="stylesheet" type="text/css" href="inner-page-style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body itemscope itemtype="http://schema.org/AboutPage">
+	<div id="page" class="site">
+		<header class="site-header">
+			<div class="top-header">
+				<div class="container">
+					<div class="top-header-left">
+						<div class="top-header-block">
+							<a href="mailto:info@cusas.com"><i class="fas fa-envelope"></i> info@cusas.com</a>
+						</div>
+						<div class="top-header-block">
+							<a href="tel:+8801685-530730"><i class="fas fa-phone"></i> +8801685-530730</a>
+						</div>
+					</div>
+					<!-- <div class="top-header-right">
+						<div class="social-block">
+							<ul class="social-list">
+								<li><a href=""><i class="fab fa-google-plus-g"></i></a></li>
+								<li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+								<li><a href=""><i class="fab fa-facebook-messenger"></i></a></li>
+								<li><a href=""><i class="fab fa-twitter"></i></a></li>
+								<li><a href=""><i class="fab fa-skype"></i></a></li>
+							</ul>
+						</div> -->
+						<div class="login-block">
+							<a href="login.html">Login /</a>
+							<a href="register.php">Register</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Top header Close -->
+			<div class="main-header">
+				<div class="container">
+					<div class="logo-wrap">
+						<img src="images/cu_logo.jpg" alt="Logo Image">
+					</div>
+					<div class="nav-wrap">
+						<nav class="nav-desktop">
+							<ul class="menu-list">
+								<li><a href="index.php">Home</a></li>
+								
+								<li><a href="about.php">About</a></li>
+								<li><a href="gallery2.php">Gallery</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+						</nav>
+						<div id="bar">
+							<i class="fas fa-bars"></i>
+						</div>
+						<div id="close">
+							<i class="fas fa-times"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- Header Close -->
+		<div class="banner">
+			<div class="owl-four owl-carousel">
+				<img src="images/pic_4.jpg" alt="Image of Bannner">
+				<img src="images/pic_3.jpg" alt="Image of Bannner">
+				<img src="images/pic_2.jpg" alt="Image of Bannner">
+			</div>
+			<div class="container">
+				<h1>Welcome to CUSAS- Chittagong University Smart Attendance System</h1>
+				<h3>"Say goodbye to manual time-keeping and hello to a smarter, more efficient future with a smart attendance system."</h3>
+			</div>
+			 <div id="owl-four-nav" class="owl-nav"></div>
+		</div>
+	
+
+		<section class="about-upper-section" itemprop="mainContentofPage">
+			<div class="container">
+				<article class="who-we-are">
+					<h2 class="top-heading">who we are</h2>
+					<p>We are a team of experienced professionals dedicated to delivering innovative and reliable database solutions.
+						 Our goal is to help organizations efficiently manage and store their data, allowing them to make informed decisions and drive growth.
+
+						With a passion for technology and a strong commitment to customer satisfaction, we are committed to delivering the highest quality products and services. 
+						Our team of experts has extensive experience in database design, development, and implementation, and we work closely with our clients to understand their unique needs and requirements.
+						
+						Whether you're looking to optimize your existing database or build a new one from scratch, we have the expertise and resources to help you achieve your goals. Our solutions are flexible, scalable,
+						 and designed to meet the evolving needs of your business.
+						
+						At our core, we believe in the power of data and the impact it can have on organizations of all sizes.
+						 We are committed to helping our clients harness this power and transform their businesses for the better.
+						
+						So if you're looking for a trusted partner to help you manage your data, 
+						look no further. Get in touch with us today to learn more about how we can help you achieve your goals.</p>
+				</article>
+				<div class="our-story">
+					<h2 class="top-heading">our story</h2>
+					<ul>
+						<li><i class="fas fa-chalkboard-teacher"></i> <a href="#">60+ teachers creates course</a></li>
+						<li><i class="fas fa-graduation-cap"></i> <a href="#">600+ students enrolled</a></li>
+						<li><i class="fas fa-users"></i> <a href="#">1000+ foreign followers</a></li>
+
+						
+					</ul>
+				</div>
+			</div>
+		</section>
+
+		<section class="team-members" itemprop="contributor">
+			<div class="container">
+				<h2 class="top-heading">meet our team</h2>
+				<article class="developer-grid">
+					<div class="developer-wrap">
+						<img src="images/aurn.jpg" alt="developer images">
+						<h3>Shanewaz Aurnob</h3>
+						<p>Student</p>
+						<ul>
+							<li><a href="https://www.facebook.com/s.aurnob"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="https://www.instagram.com/__aur_nob__/"><i class="fab fa-instagram"></i></a></li>
+							<li><a href="https://twitter.com/ShanewazAurnob"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/shanewaz-aurnob-2a49481a4/"><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+					</div>
+
+					<div class="developer-wrap">
+						<img src="images/.jpg" alt="developer images">
+						<h3>Raisa Nuzhat</h3>
+						<p>Student</p>
+						<ul>
+							<li><a href="https://www.facebook.com/profile.php?id=100078982132388"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-instagram"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+					</div>
+
+					<div class="developer-wrap">
+						<img src="images/ratri.JPG" alt="developer images">
+						<h3>Ratri Barua</h3>
+						<p>Student</p>
+						<ul>
+							<li><a href="https://www.facebook.com/nishith.aronyok.9"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-instagram"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+					</div>
+
+					<div class="developer-wrap">
+						<img src="images/jpg" alt="developer images">
+						<h3>Ramisa Zahara Matin</h3>
+						<p>Student</p>
+						<ul>
+							<li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-instagram"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+					</div>
+
+
+					
+				</article>
+			</div>
+		</section>
+	
+				</div>
+			</div>
+		</section>
+		<!-- End of Others talk -->
+
+		<section class="query-section">
+			<div class="container">
+				<p>Any Queries? Ask us a question at<a href="tel:+8801685-530730"><i class="fas fa-phone"></i> +8801685-530730</a></p>
+			</div>
+		</section>
+		<!-- End of Query Section -->
+		<footer class="page-footer">
+			<div class="footer-first-section">
+				<div class="container">
+					<div class="box-wrap">
+						<header>
+							<h1>about</h1>
+						</header>
+						<p>We believe in the power of data and the impact it can have on organizations of all sizes. Our solutions are flexible, 
+							scalable, and designed to meet the evolving needs of your business.</p>
+
+						<h4><a href="tel:+8801685-530730"><i class="fas fa-phone"></i> +8801685-530730</a></h4>
+						<h4><a href="mailto:info@cusas.com"><i class="fas fa-envelope"></i> info@cusas.com</a></h4>
+						<h4><i class="fas fa-map-marker-alt"></i>University of Chittagong, Chittagong, Bangladesh</h4>
+					</div>
+
+				
+
+				</div>
+			</div>
+			<!-- End of box-Wrap -->
+			<div class="footer-second-section">
+				<div class="container">
+					<hr class="footer-line">
+					<ul class="social-list">
+						<li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+						<li><a href=""><i class="fab fa-twitter"></i></a></li>
+						<li><a href=""><i class="fab fa-skype"></i></a></li>
+						<li><a href=""><i class="fab fa-youtube"></i></a></li>
+						<li><a href=""><i class="fab fa-instagram"></i></a></li>
+					</ul>
+					<hr class="footer-line">
+				</div>
+			</div>
+			<div class="footer-last-section">
+				<div class="container">
+					<p>Copyright 2023 &copy; arr.cu.bd.com <span> | </span> Theme designed and developed by <a href="https://ARRR LAB">ARRR LAB</a></p>
+				</div>
+			</div>
+		</footer>
+	</div>
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="js/lightbox.js"></script>
+	<script type="text/javascript" src="js/all.js"></script>
+	<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript" src="js/jquery.rateyo.js"></script>
+	<script type="text/javascript" src="js/jquery.mmenu.all.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
