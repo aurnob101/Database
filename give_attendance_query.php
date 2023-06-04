5b65301a31957e666290f16f6aa90d3176a9f848
Shanewaz Aurnob <120188817+aurnob101@users.noreply.github.com>
2023-06-04 22:49:47 +0600

    Update give_attendance_query.php
    fix bug

--- give_attendance_query.php
+++ give_attendance_query.php
@@ -5,14 +5,14 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $student_id=$_SESSION["user_id"];
     $course_code=$_POST["course_code"];
     $session_id = $_POST['session_id'];
-    // Query the session table with the entered code to get the session details
+  
     $sql_query = "SELECT * FROM `sessions` WHERE `code`='$code'";
     $result=mysqli_query($con,$sql_query);
     
     if(mysqli_num_rows($result) == 1) {
         $session=mysqli_fetch_assoc($result);
         
-        // Update a row in the attendance table
+      
        
         $date = date('Y-m-d H:i:s');
         $sql_query2 = "UPDATE `attendance` SET `status` = 'P' WHERE `attendance`.`student_id` = '$student_id' AND
